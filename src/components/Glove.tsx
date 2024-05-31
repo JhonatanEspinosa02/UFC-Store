9f1f51f0936b678f8346820a6f9253c503f372bc
Jhonatan Espinosa Monroy <jhonatan.esp21@gmail.com>
2024-05-30 22:37:13 -0600

    30/05/2024  Categories Done

--- src/components/Glove.tsx
+++ src/components/Glove.tsx
@@ -1,15 +1,54 @@
-
+import { glovesProduct } from "../data/db"
 
 function Glove() {
   return (
+<>
     <div className="p-14">
-    <h1 className="font-bold uppercase text-center text-3xl">
-      Gloves to Knock Out your rival
-    </h1>
-    <p className="font-bold text-center text-zinc-400">
-      Be stronger and toughest fighter
-    </p>
-  </div>
+          <h1 className="font-bold uppercase text-center text-3xl">
+          Gloves for the strongest kicks
+          </h1>
+          <p className="font-bold text-center text-zinc-400">
+            Knock out before the end?
+          </p>
+        </div>
+
+
+        <div className="grid grid-cols-4 items-center">
+          {glovesProduct.map((product) => (
+            <>
+              <div className="bg-slate-100 p-7 m-5 rounded-lg items-center">
+                <div>
+                  <img
+                    className="brightness-50 contrast-125 w-60 h-60 hover:filter-none rounded-lg"
+                    src={product.image}
+                    alt="product"
+                  />
+                </div>
+
+                <div className="my-7">
+                  <h2 className="font-bold text-2xl">{product.name}</h2>
+                  <p className="text-zinc-500">{product.description}</p>
+                  <img
+                    className="w-10 h-10"
+                    src="/img/2903558.png"
+                    alt="rating"
+                  />
+                  <p>$ {product.price}</p>
+                </div>
+
+                <div className="flex justify-center items-center">
+                  <a
+                    href="#"
+                    className="bg-red-600 text-white text-xl text-center rounded py-2 px-5 w-full"
+                  >
+                    Add to Cart
+                  </a>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+    </>
   )
 }
 
--- src/data/db.ts
+++ src/data/db.ts
@@ -147,6 +147,229 @@ export const helmetProduct : CategoryType[] = [
 ]
 
 
+export const shortProduct : CategoryType[] = [
+    {
+        id: 1,
+        name: "Venum Desert Camo",
+        image: "/public/img_db/Short_DesertCamo_01.webp",
+        description: "X",
+        price: 69.99
+    },
+    {
+        id: 2,
+        name: "Venum Forest Camo",
+        image: "/public/img_db/Short_ForestCamo_02.webp",
+        description: "X",
+        price: 49.99
+    },
+    {
+        id: 3,
+        name: "Venum Navy Blue",
+        image: "/public/img_db/Short_NavyBlue_03.webp",
+        description: "XL",
+        price: 79.99
+    },
+    {
+        id: 4,
+        name: "Venum Dark Camo",
+        image: "/public/img_db/Short_DarkCamo_04.webp",
+        description: "L",
+        price: 69.99
+    },
+    {
+        id: 5,
+        name: "Venum Urban Camo",
+        image: "/public/img_db/Short_UrbanCamo_05.webp",
+        description: "M",
+        price: 70
+    },
+    {
+        id: 6,
+        name: "Venum Black and White",
+        image: "/public/img_db/Short_BlackWhite_06.webp",
+        description: "XXL",
+        price: 55
+    },
+    {
+        id: 7,
+        name: "Venum Black Pink",
+        image: "/public/img_db/Short_BlackPink_07.webp",
+        description: "XS",
+        price: 55
+    },
+    {
+        id: 8,
+        name: "Venum Burgundy Gold",
+        image: "/public/img_db/Short_BurgundyGold_08.webp",
+        description: "XL",
+        price: 114.99
+    },
+    {
+        id: 9,
+        name: "Venum White Gold",
+        image: "/public/img_db/Short_WhiteGold_09.webp",
+        description: "S",
+        price: 60
+    },
+    {
+        id: 10,
+        name: "Venum Black Red",
+        image: "/public/img_db/Short_BlackRed_10.webp",
+        description: "S",
+        price: 60
+    }
+]
+
+
+
+export const glovesProduct : CategoryType[] = [
+    {
+        id: 1,
+        name: "Venum Black Silver",
+        image: "/public/img_db/Glove_BlackSilver_01.webp",
+        description: "X",
+        price: 69.99
+    },
+    {
+        id: 2,
+        name: "Venum Black Gold",
+        image: "/public/img_db/Glove_BlackGold_02.webp",
+        description: "X",
+        price: 49.99
+    },
+    {
+        id: 3,
+        name: "Venum Matte Black",
+        image: "/public/img_db/Glove_MatteBlack_03.webp",
+        description: "XL",
+        price: 79.99
+    },
+    {
+        id: 4,
+        name: "Venum Matte Black 2",
+        image: "/public/img_db/Glove_MatteBlack2_04.webp",
+        description: "L",
+        price: 69.99
+    },
+    {
+        id: 5,
+        name: "Venum Black Silver 2",
+        image: "/public/img_db/Glove_Black_05.webp",
+        description: "M",
+        price: 70
+    },
+    {
+        id: 6,
+        name: "Venum Contender Black",
+        image: "/public/img_db/Glove_ContenderBlack_06.webp",
+        description: "XXL",
+        price: 55
+    },
+    {
+        id: 7,
+        name: "Venum Contender White",
+        image: "/public/img_db/Glove_ChallengerWhite_07.webp",
+        description: "XS",
+        price: 55
+    },
+    {
+        id: 8,
+        name: "Venum Black Kid",
+        image: "/public/img_db/Glove_ChallengerBlackKid_08.webp",
+        description: "XL",
+        price: 114.99
+    },
+    {
+        id: 9,
+        name: "Venum Contender Pink",
+        image: "/public/img_db/Glove_ContenderPink_09.webp",
+        description: "S",
+        price: 60
+    },
+    {
+        id: 10,
+        name: "Venum Evo Bronze",
+        image: "/public/img_db/Gloves_EvoBronze_10.webp",
+        description: "S",
+        price: 60
+    }
+]
+
+
+
+export const accessorieProduct : CategoryType[] = [
+    {
+        id: 1,
+        name: "Venum Ankle Red and Black",
+        image: "/public/img_db/Accessorie_KontactAnkle_RedBlack_01.webp",
+        description: "X",
+        price: 69.99
+    },
+    {
+        id: 2,
+        name: "Venum Ankle Pink and White",
+        image: "/public/img_db/Accessorie_KontactAnkle_PinkWhite_02.webp",
+        description: "X",
+        price: 49.99
+    },
+    {
+        id: 3,
+        name: "Venum Ankle Black and White",
+        image: "/public/img_db/Accessorie_KontactAnkle_BlackWhite_03.jpg",
+        description: "XL",
+        price: 79.99
+    },
+    {
+        id: 4,
+        name: "Venum Hand Black and Gold",
+        image: "/public/img_db/Accessorie_KontactHand_BlackGold_04.webp",
+        description: "L",
+        price: 69.99
+    },
+    {
+        id: 5,
+        name: "Venum Kontac Hand Black and White",
+        image: "/public/img_db/Accessorie_KontactHand_BlackWhite_05.jpg",
+        description: "M",
+        price: 70
+    },
+    {
+        id: 6,
+        name: "Venum Kontac Hand Blue",
+        image: "/public/img_db/Accessorie_KontactHand_Blue_06.webp",
+        description: "XXL",
+        price: 55
+    },
+    {
+        id: 7,
+        name: "Venum Kontac Hand Red",
+        image: "/public/img_db/Accessorie_KontactHand_Red_07.webp",
+        description: "XS",
+        price: 55
+    },
+    {
+        id: 8,
+        name: "Venum Kontac Shin Black",
+        image: "/public/img_db/Accessorie_KontactShin_Black_08.webp",
+        description: "XL",
+        price: 114.99
+    },
+    {
+        id: 9,
+        name: "Venum Kontac Elbow Black",
+        image: "/public/img_db/Accessorie_KontactElbow_Black_09.jpg",
+        description: "S",
+        price: 60
+    },
+    {
+        id: 10,
+        name: "Venum Kontac Knee Black",
+        image: "/public/img_db/Accessorie_KontactKnee_Black_10.webp",
+        description: "S",
+        price: 60
+    }
+]
+
 
 
 
